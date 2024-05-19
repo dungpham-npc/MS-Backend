38e4eedca0016dc44b7e56ebe500b17b39b25215
Dung Pham <anhdungp0304@gmail.com>
2024-05-19 17:01:14 +0700

    Initialize project

---
+++ milkstore/src/test/java/com/cookswp/milkstore/MilkstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cookswp.milkstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilkstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
