30889842de35a0009cef9bed285439f5f8983ca6
Dung Pham <anhdungp0304@gmail.com>
2024-05-31 14:23:03 +0700

    Update User.java

--- milkstore/src/main/java/com/cookswp/milkstore/pojo/dtos/UserModel/User.java
+++ milkstore/src/main/java/com/cookswp/milkstore/pojo/dtos/UserModel/User.java
@@ -1,6 +1,6 @@
 package com.cookswp.milkstore.pojo.dtos.UserModel;
 
-import com.cookswp.milkstore.pojo.dtos.UserModel.Role;
+import com.cookswp.milkstore.pojo.dtos.RoleModel.Role;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
