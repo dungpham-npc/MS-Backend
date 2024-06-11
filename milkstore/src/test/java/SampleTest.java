cfef6110ee078356403352608095616a4f9909f6
Dung Pham <anhdungp0304@gmail.com>
2024-06-11 18:39:43 +0700

    Create SampleTest.java

---
+++ milkstore/src/test/java/SampleTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SampleTest {
+
+    @Test
+    public void testAddition() {
+        int result = 2 + 3;
+        assertEquals(5, result, "2 + 3 should equal 5");
+    }
+
+    @Test
+    public void testStringConcatenation() {
+        String result = "Hello" + " " + "World";
+        assertEquals("Hello World", result, "Concatenation failed");
+    }
+}
+
