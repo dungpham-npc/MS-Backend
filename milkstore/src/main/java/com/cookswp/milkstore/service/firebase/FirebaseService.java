4459b2c0e7ac1aa602bd43df3cf5e809a8fea979
Hoang Hai Hau <hhhau1910@gmail.com>
2024-07-16 14:40:02 +0700

    commit gitignore

--- milkstore/src/main/java/com/cookswp/milkstore/service/firebase/FirebaseService.java
+++ milkstore/src/main/java/com/cookswp/milkstore/service/firebase/FirebaseService.java
@@ -6,13 +6,14 @@ import com.google.cloud.storage.BlobId;
 import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import io.github.cdimascio.dotenv.Dotenv;
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -22,14 +23,15 @@ import java.util.UUID;
 public class FirebaseService {
 
     private String uploadFile(File file, String fileName) throws IOException {
-        BlobId blobId = BlobId.of("firebase-service-427505.appspot.com", fileName);
+        BlobId blobId = BlobId.of("swp-firebase-8e460.appspot.com", fileName);
         BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType("media").build();
-        InputStream inputStream = FirebaseService.class.getClassLoader().getResourceAsStream("firebase-private-key.json");
+        InputStream inputStream = FirebaseService.class.getClassLoader().getResourceAsStream("swp-firebase-adminsdk.json");
+        assert inputStream != null;
         Credentials credentials = GoogleCredentials.fromStream(inputStream);
         Storage storage = StorageOptions.newBuilder().setCredentials(credentials).build().getService();
         storage.create(blobInfo, Files.readAllBytes(file.toPath()));
 
-        String DOWNLOAD_URL = "https://firebasestorage.googleapis.com/v0/b/firebase-service-427505.appspot.com/o/%s?alt=media";
+        String DOWNLOAD_URL = "https://firebasestorage.googleapis.com/v0/b/swp-firebase-8e460.appspot.com/o/%s?alt=media";
         return String.format(DOWNLOAD_URL, URLEncoder.encode(fileName, StandardCharsets.UTF_8));
     }
 
